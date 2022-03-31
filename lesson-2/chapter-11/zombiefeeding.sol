232d86b4a42dc05868d101a437d21dcd56851635
go2kiran <97989757+go2kiran@users.noreply.github.com>
2022-03-31 16:17:11 -0500

    Chapter11_1

--- lesson-2/chapter-11/zombiefeeding.sol
+++ lesson-2/chapter-11/zombiefeeding.sol
@@ -1,5 +1,7 @@
-pragma solidity ^0.4.25;
+pragma solidity >=0.5.0 <0.6.0;
+
 import "./zombiefactory.sol";
+
 contract KittyInterface {
   function getKitty(uint256 _id) external view returns (
     bool isGestating,
@@ -14,10 +16,12 @@ contract KittyInterface {
     uint256 genes
   );
 }
+
 contract ZombieFeeding is ZombieFactory {
 
   address ckAddress = 0x06012c8cf97BEaD5deAe237070F9587f8E7A266d;
   KittyInterface kittyContract = KittyInterface(ckAddress);
+  
 
   function feedAndMultiply(uint _zombieId, uint _targetDna) public {
     require(msg.sender == zombieToOwner[_zombieId]);
