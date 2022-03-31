6ed1aacb835ef526f82f3f9b57b5626be5a6af08
go2kiran <97989757+go2kiran@users.noreply.github.com>
2022-03-31 16:06:14 -0500

    Chapter8_1

--- lesson-2/chapter-8/zombiefeeding.sol
+++ lesson-2/chapter-8/zombiefeeding.sol
@@ -1,5 +1,7 @@
-pragma solidity ^0.4.25;
+pragma solidity >=0.5.0 <0.6.0;
+
 import "./zombiefactory.sol";
+
 contract ZombieFeeding is ZombieFactory {
 
   function feedAndMultiply(uint _zombieId, uint _targetDna) public {
@@ -7,7 +9,7 @@ contract ZombieFeeding is ZombieFactory {
     Zombie storage myZombie = zombies[_zombieId];
     _targetDna = _targetDna % dnaModulus;
     uint newDna = (myZombie.dna + _targetDna) / 2;
-    _createZombie("NoName", newDna);
+    _createZombie("NoName",newDna);
   }
 
 }
